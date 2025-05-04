6f2522064ef509afd5788da1a60ff8ad4f12a7c4
LJ <ljdb1999@gmail.com>
2025-05-04 11:28:41 -0700

    Episode 2 grid building

---
+++ Assets/Builder/BuildingSystem.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSystem : MonoBehaviour
+{
+    public List<buildObjects> objects = new List<buildObjects>();
+    public buildObjects currentobject;
+    private Vector3 currentpos;
+    public Transform currentPreview;
+    public Transform cam;
+    public RaycastHit hit;
+    public LayerMask layer;
+
+    public float offset = 1.0f;
+    public float gridSize = 1.0f;
+
+    public bool isBuilding = false;
+    void Start()
+    {
+        currentobject = objects[0];
+        ChangeCurrentBuilding();
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if(isBuilding)
+        {
+            startPreview();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            isBuilding = !isBuilding;
+            if (isBuilding && currentobject != null)
+            {
+                ChangeCurrentBuilding(); // Instantiate preview when building starts
+            }
+        }
+    }
+
+    public void ChangeCurrentBuilding()
+    {
+        GameObject curprev = Instantiate(currentobject.preview, currentpos, Quaternion.identity) as GameObject;
+        currentPreview = curprev.transform;
+    }
+
+    public void startPreview()
+    {
+        if(Physics.Raycast(cam.position, cam.forward, out hit, 20, layer))
+        {
+            if (hit.transform != this.transform)
+                showPreview(hit);
+        }
+    }
+
+    public void showPreview(RaycastHit hit2)
+    {
+        currentpos = hit2.point;
+        currentpos -= Vector3.one * offset;
+        currentpos /= gridSize;
+        currentpos = new Vector3(Mathf.Round(currentpos.x), Mathf.Round(currentpos.y), Mathf.Round(currentpos.z));
+        currentpos *= gridSize;
+        currentpos += Vector3.one * offset;
+        currentPreview.position = currentpos;
+    }
+}
+
+[System.Serializable]
+public class buildObjects
+{
+    public string name;
+    public GameObject preview;
+    public int gold;
+}
+
+
---
+++ Assets/Builder/previewObject.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class previewObject : MonoBehaviour
+{
+    public bool foundation;
+    public List<Collider> col = new List<Collider>();
+    public Material green;
+    public Material red;
+    public bool isBuildable;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9 && foundation)
+            col.Add(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 9 && foundation)
+            col.Remove(other);
+    }
+
+    public void changecolor()
+    {
+        if (col.Count == 0)
+            isBuildable = true;
+        else
+            isBuildable = false;
+
+        if (isBuildable)
+            foreach(Transform child in this.transform)
+            {
+                child.GetComponent<Renderer>().material = green;
+            }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
